cad12ce16ecad271f487db2983f456a37bc9197f
Alexander Daza <dev.alexander.daza@gmail.com>
2023-01-13 12:20:46 -0500

    feat: implemented character details showed on modal box

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,22 @@ import { makeRequest } from './helpers/index';
 function App() {
 
   const [rickAndMortyCharacters, setRickAndMortyCharacters] = useState<any>([])
+  const [rickAndMortyCharacter, setRickAndMortyCharacter] = useState<any>([])
 
   useEffect(() => {
     makeRequest('rick-and-morty-characters', {
       method: 'GET'
     }).then((response: any) => {
-      console.log(response)
       setRickAndMortyCharacters(response.results)
     });
   }, [])
 
   const getCharacterDetails = (id: number) => {
+    setRickAndMortyCharacter([]);
     makeRequest(`rick-and-morty-character/${id}`, {
       method: 'GET'
     }).then((response: any) => {
-      console.log(response)
+      setRickAndMortyCharacter(response);
     });
   }
 
@@ -45,14 +46,44 @@ function App() {
                     <li className="list-group-item">Born in {character.origin.name}</li>
                   </ul>
                 </p>
-                <div className='card-footer' onClick={e => getCharacterDetails(character.id)}>
-                  <button>View More</button>
+                <div className='card-footer'>
+                  <button data-bs-toggle="modal" data-bs-target='#charModal' onClick={e => getCharacterDetails(character.id)}>View More</button>
                 </div>
               </div>
             </div>
           </div>
         })}
-      </div>      
+      </div>
+      <div className="modal fade" id="charModal" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel" style={{ color: 'black' }}>{rickAndMortyCharacter.name}</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+            <img src={rickAndMortyCharacter.image} className="card-img-top"
+                alt="Hollywood Sign on The Hill" />
+              <strong style={{ color: 'black' }}>Name:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.name}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Gender:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.gender}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Species:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.species}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Status:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.status}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Episodes:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.episode?.length}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Location:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.location?.name}</span>
+              <br />
+              <strong style={{ color: 'black' }}>Origin:</strong> <span style={{ color: 'gray' }}>{rickAndMortyCharacter.origin?.name}</span>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <FooterComponent></FooterComponent>
     </div>
   )
